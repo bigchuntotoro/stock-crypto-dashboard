080442b5849c338a502ed76eea20d99432791b44
totoro <bigchun@hanmail.net>
2026-09-17 16:57:54 +0900

    수정4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
-import "./styles/dashboard.css";
+import "./styles/Dashboard.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
